036eb788f0ad386295b367cf3a5640a142a6bfe7
Daniel Abati <81102803+Night-Prime@users.noreply.github.com>
2022-07-01 14:58:31 +0100

    working with Observables

--- Ang-app/src/app/app.component.ts
+++ Ang-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ang-app';
+
+  constructor() {
+    type HttpResponse = { code: number , data: string};
+
+    const observable = new Observable<HttpResponse>(subscriber => {
+      console.log('Test-Subscriber');
+      subscriber.next({code: 500, data: 'First Data.....'});
+      subscriber.next({code: 400, data: 'Second Data.....'});
+      subscriber.next({code: 300, data: 'Third Data.....'});
+      subscriber.next({code: 200, data: 'Fourth Data.....'})
+      setTimeout(() => {
+        subscriber.next({code: 100, data: 'Fifth Data.....'});
+        subscriber.complete();
+      }, 5000);
+      console.log('Susbscriber stops here!');
+    });
+
+    observable.subscribe({
+      next(response: HttpResponse) {
+        console.log('got my response: ', response);
+      },error(error:any){
+          
+      }
+    })
+
+  }
 }
+
