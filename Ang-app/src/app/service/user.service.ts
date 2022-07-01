a3e2d78b1d01e418e2e3f588da950210972f40dd
Daniel Abati <81102803+Night-Prime@users.noreply.github.com>
2022-07-01 12:54:06 +0100

    Angular-Http-start

---
+++ Ang-app/src/app/service/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getPlaylist(): Observable<Playlist[]> {
+    return this.http.get<Playlist[]>(`https://my-json-server.typicode.com/Night-Prime/Playlist-API/playlist`);
+  }
+
+  getSong(): Observable<Playlist> {
+    return this.http.get<Playlist>(`https://my-json-server.typicode.com/Night-Prime/Playlist-API/playlist/1`);
+  }
+}
